ebe6b28a29ae05541dfdc215958e0ceb601e81e9
Jerick333 <povdavin96@gmail.com>
2025-01-30 13:51:50 +0700

    davin

--- src/Account.java
+++ src/Account.java
@@ -1,12 +1,12 @@
 public class Account {
     private static int accountCounter = 0;
-    private int accountID;
-    private String accountType;     //saving or checking
+    int accountID;
+    String accountType;     //saving or checking
     private double balance;
-    private String currencyType;
-    private String dateCreated;
-    private String status;          //active or inactive
-    private int customerID;
+    String currencyType;
+    String dateCreated;
+    String status;          //active or inactive
+    int customerID;
 
     public Account(String accountType, double balance, String currencyType, String dateCreated, String status, int customerID) {
         this.accountID = ++accountCounter;
@@ -18,51 +18,11 @@ public class Account {
         this.customerID = customerID;
     }
 
-    public int getAccountID() {
-        return accountID;
-    }
-
-    public String getAccountType() {
-        return accountType;
-    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public String getCurrencyType() {
-        return currencyType;
-    }
-
-    public String getDateCreated() {
-        return dateCreated;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public int getCustomerID() {
-        return customerID;
-    }
-
-    public void setAccountType(String accountType) {
-        this.accountType = accountType;
-    }
-
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
-
-    public void setCurrencyType(String currencyType) {
-        this.currencyType = currencyType;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public void setCustomerID(int customerID) {
-        this.customerID = customerID;
-    }
+    // public double getBalance () {
+    //     return balance ;
+    // }
+    
+    // public void setBalance(double balance){
+    //     this.balance = balance;
+    // }
 }
